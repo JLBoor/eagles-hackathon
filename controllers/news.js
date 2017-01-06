5fe0986f5b693558a66459cae5bacf8cbc950c22
Jean Bourgeois <BourgeoisJ@intelliware.ca>
2017-01-06 16:36:01 -0500

    Cleaned up titles

--- controllers/news.js
+++ controllers/news.js
@@ -6,9 +6,14 @@
   var googleFinance = require('google-finance');
 
   exports.get = (req, res) => {
+
+    if(!req.query.symbol) {
+      return [];
+    }
+
     googleFinance.companyNews({
       symbol: req.query.symbol
     }, function (err, news) {
-      res.send(news);
+      res.send(news.splice(0, 5));
     });
   };
