41868eb74ad7c68c02c2ff816d7e459e09fd1d52
Manea Marius <maneamarius@gmail.com>
2017-01-06 10:48:36 -0500

    add python script for uploading data

---
+++ data-uploader.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+import requests
+
+dataFile="../rbc_sectran.csv"
+url="http://localhost:3000/api/transactions"
+
+def postData( line ):
+    try:
+        requests.post(url, data={'csvLine': line})
+    except ZeroDivisionError:
+        print "*** failed ***"
+
+i=0
+with open(dataFile) as f:
+    for line in f:
+        postData(line)
+        i=i+1
+        print "Posted: " + str(i)
+
