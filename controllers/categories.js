046fab1992ba3b5af73406279b36dc4650276827
Jean Bourgeois <BourgeoisJ@intelliware.ca>
2017-01-06 09:52:44 -0500

    Added Categories Pie Chart

---
+++ controllers/categories.js
@@ -0,0 +1,40 @@
+/**
+ * GET /categories
+ */
+
+const Transaction = require('../models/Transaction');
+
+exports.settlementCurrency = (req, res) => {
+
+  var client = req.query.client;
+
+  Transaction.aggregate([
+          { $project: {'Settlement_Currency': 1, 'Counterparty_Name':1} },
+          { $match: { 'Counterparty_Name': {$exists: true, $nin: ['']}, 'Counterparty_Name': client}},
+          { $group : {_id:'$Settlement_Currency', count:{$sum:1}}}],
+        function (err, result) {
+          if (err) {
+            console.log(err);
+            return;
+        }
+        res.send(result);
+    });
+};
+
+
+exports.securityType = (req, res) => {
+
+  var client = req.query.client;
+
+  Transaction.aggregate([
+          { $project: {'Security_Type_Description': 1, 'Counterparty_Name':1} },
+          { $match: { 'Counterparty_Name': {$exists: true, $nin: ['']}, 'Counterparty_Name': client}},
+          { $group : {_id:'$Security_Type_Description', count:{$sum:1}}}],
+        function (err, result) {
+          if (err) {
+            console.log(err);
+            return;
+        }
+        res.send(result.map((type) => [type._id, type.count]));
+    });
+};
