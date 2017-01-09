75869624a86189295a0f5f52662b177976e34958
Jean Bourgeois <BourgeoisJ@intelliware.ca>
2017-01-09 09:55:26 -0500

    Added  customizable page

--- controllers/home.js
+++ controllers/home.js
@@ -3,6 +3,12 @@
  * Home page.
  */
 
+ exports.customized = (req, res) => {
+   res.render('customized', {
+     title: 'Customized'
+   });
+ };
+
 exports.quickQuote = (req, res) => {
   res.render('quick-quote', {
     title: 'Quick Quote'
